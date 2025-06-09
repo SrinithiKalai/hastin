c705ca4382307f91ae5427d2c0aaac5e1b3a0f80
SrinithiKalai <srinithikalai2@gmail.com>
2025-06-09 14:27:00 +0530

    api

--- src/App.js
+++ src/App.js
@@ -1,61 +1,17 @@
-// import "bootstrap/dist/css/bootstrap.min.css";
-// import {BrowserRouter, Routes, Route} from 'react-router-dom';
-// import HastinLogin from "./react-api/HastinLogin";
-// import HastinTable from"./react-api/HastinTable"
-
-// <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"/>
-// function App() {
-//   return (
-//     <div>
-//       <BrowserRouter>
-//       <Routes>
-//      <Route path="/" element={<HastinLogin/>}/>
-//      <Route path="table/" element={<HastinTable/>}/>
-//       </Routes>
-//       </BrowserRouter>
-//     </div>
-//   );
-// }
-
-// export default App;
-
 import "bootstrap/dist/css/bootstrap.min.css";
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import {BrowserRouter, Routes, Route} from 'react-router-dom';
 import HastinLogin from "./react-api/HastinLogin";
-import HastinTable from "./react-api/HastinTable";
-import CreateTable from "./react-api/CreateTable";
-import Vendor from "./react-api/Vendor";
-import React, { useState } from "react";
+import HastinTable from"./react-api/HastinTable"
 
-// Remove <link .../> from here, use it in public/index.html if needed
-
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/6.0.0/css/all.min.css"/>
 function App() {
-  const [table, setTable] = useState('table');
-  const [selectedVendor, setSelectedVendor] = useState(null);
-
-  const handleSetTable = (view, vendor) => {
-    setTable(view);
-    setSelectedVendor(vendor || null);
-  };
-
   return (
     <div>
       <BrowserRouter>
-        <Routes>
-          <Route path="/" element={<HastinLogin />} />
-          <Route path="table/" element={<HastinTable />} />
-          {/* If you want to use SPA navigation for CreateTable & Vendor */}
-          <Route
-            path="/vendors"
-            element={
-              table === 'table' ? (
-                <CreateTable setTable={handleSetTable} />
-              ) : (
-                <Vendor selectedVendor={selectedVendor} isEditMode={!!selectedVendor} />
-              )
-            }
-          />
-        </Routes>
+      <Routes>
+     <Route path="/" element={<HastinLogin/>}/>
+     <Route path="table/" element={<HastinTable/>}/>
+      </Routes>
       </BrowserRouter>
     </div>
   );
